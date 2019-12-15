b230a44b239102fe68df833d3ddd48ea5aae7d47
agnieszka <agnieszka.gfkm@o2.pl>
2019-12-15 11:46:28 +0100

    [aa] - address domain object

---
+++ src/main/java/pl/aga/springwithgit/domain/Address.java
@@ -0,0 +1,16 @@
+package pl.aga.springwithgit.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Address {
+    private String city;
+    private String street;
+    private int flatNumber;
+
+
+
+}
